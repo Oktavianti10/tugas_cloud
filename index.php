0e2294ebab530e6267b8bbcedc4266acebccd814
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-21 22:09:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<img src="kot-koshka-minimalizm-sidit.jpg">
+<img src="https://github.com/Oktavianti10/tugas_cloud/blob/master/b29895a51d92b3fb0b7153af99b5ead1.png">
 <h1>My First Heading</h1>
 
 <p>My first paragraph.</p>
