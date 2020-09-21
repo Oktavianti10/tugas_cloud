f07429f6cf7c6e9d98b8c3759903c1fefc109557
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-21 22:10:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<img src="https://github.com/Oktavianti10/tugas_cloud/blob/master/b29895a51d92b3fb0b7153af99b5ead1.png">
+<img src="b29895a51d92b3fb0b7153af99b5ead1.png">
 <h1>My First Heading</h1>
 
 <p>My first paragraph.</p>
