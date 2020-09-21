f391ebc635a0f6e6815f3f6bd20fd7c80c93712f
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-21 22:35:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+body {
+  background-image: url('b29895a51d92b3fb0b7153af99b5ead1.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+</head>
 <body>
-<img src="b29895a51d92b3fb0b7153af99b5ead1.png">
-<h1>My First Heading</h1>
 
-<p>My first paragraph.</p>
+<h2>Background Cover</h2>
+
+<p>Set the background-size property to "cover" and the background image will cover the entire element, in this case the body element.</p>
 
 </body>
 </html>
