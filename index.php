01c9f361ef61832ac36b790bbd85970dce4c89db
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-24 10:22:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,6 @@ body {
   
 <body>
  <picture>
-  <source media="(min-width: 650px)" srcset="ib29895a51d92b3fb0b7153af99b5ead1.png">
-  <source media="(min-width: 465px)" srcset="ib29895a51d92b3fb0b7153af99b5ead1.png">
   <img src="ib29895a51d92b3fb0b7153af99b5ead1.png">
 </picture>
   
