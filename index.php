6bd8208678c4d0d1171ebf8353ce17be2bc745a6
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-24 10:13:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ body {
   background-attachment: fixed;  
   background-size: cover;
 }
-  <center>
-  <img src = "b29895a51d92b3fb0b7153af99b5ead1.png">
-  </center>
-  
 </style>
 </head>
+  
 <body>
-
+  <center>
+  <img src= "b29895a51d92b3fb0b7153af99b5ead1.png">
+  </center>
+  
 <h2>Background Cover</h2>
 
 <p>Set the background-size property to "cover" and the background image will cover the entire element, in this case the body element.</p>
