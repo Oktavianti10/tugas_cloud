996707fa0b1c938413d08e750b71985f5bf3485a
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-27 22:59:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,180 @@
 <!DOCTYPE html>
-<html>
-<style>
-body 
-    {
-        background: url('pastel.jpg');
-	font-family: serif;
-    } 
-</style>
-<body>
-
-<h1>My First Heading</h1>
-
-<p>My first paragraph.</p>
-
-</body>
+<html lang="en">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <title>Erwinda Oktavianti N</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+  font: 14px "Montserrat", sans-serif;
   
+}
+
+p {font-size: 14px;}
+  </style>
+  
+  <style>
+  .bg-1 {
+    background-color: #d5869d ;  /* Light Steel Blue */
+    color: #000000; /* Black */
+  }
+  </style>
+  <style>
+  .bg-2 {
+    background-color: #E6E6FA ; /* Lavender */
+    color: #000000; /* Black*/
+  }
+  </style>
+  <style>
+    .bg-3 {
+    background-color: #E6E6FA; /* Lavender */
+    color: #000000; /* Black*/
+  }
+  </style>
+  
+   <style>
+.bg-4 {
+  background-color:  #B0C4DE ; /* Light Steel Blue */
+  color: #000000; /* Black*/
+}
+</style>
+
+</head>
+<body>
+<style>
+.container-fluid {
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+</style>
+  <style>
+  .navbar {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border: 0;
+  border-radius: 0;
+  margin-bottom: 0;
+  font-size: 12px;
+  letter-spacing: 5px;
+}
+     </style>
+  <style>
+.navbar-nav li a:hover {
+  color: #B0C4DE !important;
+}
+  </style>
+  <nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Andira Pauline</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Cerita</a></li>
+        <li><a href="#">Materi & Tugas</a></li>
+        <li><a href="#">Tentang Saya</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+  <div class="container-fluid bg-1 text-center">
+    <h3>Halo Semua</h3> 
+  <br>
+  <img src="WFH.jpg" class="img-rounded" alt="WFH" width="220" height="350">
+  </br>
+  <br>
+  <h3>Selamat Datang</h3>
+  </br>
+</div>
+  
+  <div class="container-fluid bg-2 text-center">
+    <br>
+  <h3>--Suasana Work From Home--</h3>
+    </br>
+<p><br>Assalamualaikum Warahmatullahi Wabarakatuh</br>
+
+<br>
+Pengalaman saya selama kuliah dari rumah atau Work from home (WFH) tentu ada menarik dan tidaknya, kalau menurut saya menariknya WFH ini karena bisa tetap dirumah bahkan setelah kuliah selesai bisa langsung beristirahat. Selain itu WFH juga menyenangkan karena bisa mengerjakan pekerjaan rumah dan tugas kuliah sekaligus, dengan WFH ini saya belajar untuk membagi waktu dan waktu sangat berguna karena mengerjakan
+beberapa tugas sekaligus, misalnya setelah kuliah selesai saya bisa mengerjakan pekerjaan rumah terlebih dulu seperti beres-beres rumah dan lainnya seperti tugas anak perempuan pada umumnya.
+Lalu tugas kuliah saya kerjakan biasanya tugas yang lebih dahulu mau dikumpulkan, biasanya sih saya mengerjakan tugas waktu malam hari bahkan bisa sampai larut malam untuk tugas yang waktunya dekat dengan waktu pengumpulan.
+Mengerjakan tugas-tugas juga saya biasa berdiskusi dengan teman kelompok, karena sedang WFH jadi diskusi dilakukan secara daring lewat zoom atau google meet.
+</br>
+<br>
+Liburan saya saat WFH juga cukup menyenangkan karena tidak bisa keluar rumah jadi biasanya sabtu-minggu saat libur kuliah saya gunakan untuk hobi saya, seperti mengerjakan projek tulisan cerita,
+menonton drama korea dan biasanya dihalaman rumah saya berolahraga dengan adik-adik memainkan olahraga bulutangkis.
+Selama WFH juga saya dapat membantu adik-adik saya mengerjakan tugas dari sekolahnya dan mengajarkan materi pelajaran yang mereka tidak pahami.
+</br>
+<br>
+Kurang lebih sudah 7 bulan WFH, jujur saja tidak menariknya WFH mungkin bosan karena melakukan aktifitas yang monoton dirumah setiap hari, tidak bisa bertemu dan kumpul dengan teman-teman, apalagi saya belum sempat untuk
+mengikuti organisasi dikampus. saya juga sudah rindu dengan teman-teman kelas, rindu lingkungan kampus juga. Saya berharap semester depan sudah bisa normal kembali.</br></p>
+</div>
+  
+ <div class="container-fluid bg-3 text-center">    
+  <h3>--Fasilitas UIN Syarif Hidayatullah Jakarta--</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Fasilitas utama menurut saya yaitu fakultas, ini gambar fakultas sains dan teknologi. Selain terdapat ruang kelas sebagai tempat belajar didalam fakultas ini juga banyak fasilitas pendukung lain misalnya,
+      mushollah, kantin dan terdapat juga gazebo yang letaknya ditengah fakultas biasanya juga disebut saung. Lobby fakultas juga dimanfaatkan mahasiswa untuk berkumpul, mengobrol sampai mengerjakan tugas</p>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <img src="OIP.jpg" class="img-rounded" alt="WFH" width="350" height="220" alt="OIP">
+      </br>
+    </br>
+   </br>
+</br>
+</br>
+    </div>
+    <div class="col-sm-4"> 
+      <p>Fasilitas lain dari UIN Jakarta yaitu Perpustakaan. Perpustakaan UIN Jakarta sangat luas dan tersedia berbagai macam buku sampai sekumpulan skripsi mahasiswa, selain untuk tempat belajar dan mencari buku
+      biasanya perpustakaan bekerjasama dengan Gramedia atau toko buku lainnya untuk mengadakan bazar buku murah dan alat-alat tulis. Suasana diperpustakaan sangat nyaman dan hening cocok sebagai tempat mengerjakan tugas</p>
+     <br>
+      <br>
+      <br> 
+      <br>
+      <img src="perpustakaan.jpg" class="img-rounded" alt="WFH" width="350" height="220" alt="perpustakaan">
+      </br> 
+    </br>
+</br> 
+</br>
+    </div>
+    <div class="col-sm-4"> 
+      <p>Fasilitas yang salah satunya baik menurut saya yaitu RS UIN. karena kampus memiliki rumah sakit sendiri yang digunakan untuk warga kampus maupun masyarakat diluar kampus. Sepengalaman saya menggunakan
+      fasilitas ini juga sangat mudah dan nyaman, bagi saya yang seorang mahasiswa untuk berobat ke RS UIN ini cukup menggunakan kartu mahasiswa dan menunjukan slip pembayaran semester terakhir, lalu saya mengatakan akan menemui dokter sesuai keluhan.
+      Setelah itu hanya menunggu giliran masuk untuk diperiksa dokter dan sesudahnya hanya tinggal menunggu resep obat dari apoteknya.</p>
+      <br>
+      <img src="rsuin.jpg" class="img-rounded" alt="WFH" width="350" height="220" alt="rsuin">
+      </br>
+    </div>
+  </div>
+</div>
+  
+ 
+<footer class="container-fluid bg-4 text-center">
+  <p>My Social Media</p>
+  <br>
+  Instagram & Twitter
+  <br>
+  Andirapauline
+  <br>
+  :)
+  </br>
+  </br>
+  </br>
+</footer>
+</body>
 </html>
