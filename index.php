e8ae42db8aa8156d9f418bcdc0062aa36dae3787
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-29 16:27:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -74,8 +74,8 @@ p {font-size: 14px;}
 </style>
   <style>
   .navbar {
-  padding-top: 30px;
-  padding-bottom: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
   border: 0;
   border-radius: 0;
   margin-bottom: 0;
