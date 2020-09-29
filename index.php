e6bb1e6964aca75ed0b464fd93fafc333d75a22c
Oktavianti10 <71652383+Oktavianti10@users.noreply.github.com>
2020-09-29 13:53:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ p {font-size: 14px;}
   <style>
   .bg-1 {
     background-image: url('uin3.jpg');  /* pink */
-    background-size: width=200 height=200;
     background-repeat: no-repeat;
     color: #000000; /* Black */
   }
